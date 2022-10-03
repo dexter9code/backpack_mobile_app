651a2de2e5673a0251c9a59ad1d7dd0e7c4cf492
rudy <doomdexter07@gmail.com>
2022-10-03 23:23:45 +0530

    setup the initial project

---
+++ components/colors.js
@@ -0,0 +1,14 @@
+export const colors = {
+  pink500: "#FF577F",
+  pink700: "#F96666",
+  purple500: "#937DC2",
+  purple700: "#6F38C5",
+  green500: "#59CE8F",
+  green300: "#8FE3CF",
+  dodgerBlue500: "#47B5FF",
+  dodgerBlue700: "#31E1F7",
+  gray500: "#AEBDCA",
+  gray300: "#D8D8D8",
+  white500: "#F9F9F9",
+  white600: "#EAF6F6",
+};
